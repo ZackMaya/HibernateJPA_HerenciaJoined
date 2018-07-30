f4bd0e488246486b3ed8563fad9f3d1088e7cfc5
ZackMaya <ingsoftnetozack@outlook.com>
2018-07-30 17:05:55 -0500

    Primer Commit

---
+++ src/main/java/mx/edu/uacm/herencia/HibernateJpaHerenciaJoinedApplication.java
@@ -0,0 +1,12 @@
+package mx.edu.uacm.herencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaHerenciaJoinedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaHerenciaJoinedApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/edu/uacm/herencia/HibernateJpaHerenciaJoinedApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.edu.uacm.herencia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibernateJpaHerenciaJoinedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
